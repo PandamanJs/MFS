7458ad3d32d212aa68b936ca5a8be908776a6c63
PandamanJs <176211987+PandamanJs@users.noreply.github.com>
2025-09-16 19:02:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
